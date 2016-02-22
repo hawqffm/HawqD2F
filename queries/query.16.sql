c6630347af31bb582ea86f653a12099a075413bc
hawqffm <apfelkekskuchen@hotmail.de>
2016-02-22 13:10:43 +0100

    Added files via upload

---
+++ queries/query.16.sql
@@ -0,0 +1,51 @@
+-- create the result table
+drop table if exists q16_parts_supplier_relationship;
+create table q16_parts_supplier_relationship(p_brand text, p_type text, p_size int, supplier_cnt int);
+drop table if exists q16_tmp;
+create table q16_tmp(p_brand text, p_type text, p_size int, ps_suppkey int);
+drop table if exists supplier_tmp;
+create table supplier_tmp(s_suppkey int);
+
+
+-- the query
+insert into supplier_tmp
+select 
+  s_suppkey
+from 
+  supplier
+where 
+  not s_comment like '%Customer%Complaints%';
+
+insert into q16_tmp
+select 
+  p_brand, p_type, p_size, ps_suppkey
+from 
+  partsupp ps join part p 
+  on 
+    p.p_partkey = ps.ps_partkey and p.p_brand <> 'Brand#34' 
+    and not p.p_type like 'ECONOMY BRUSHED%'
+  join supplier_tmp s 
+  on 
+    ps.ps_suppkey = s.s_suppkey;
+
+insert into q16_parts_supplier_relationship
+select 
+  p_brand, p_type, p_size, count(distinct ps_suppkey) as supplier_cnt
+from 
+  (select 
+     * 
+   from
+     q16_tmp 
+   where p_size = 22 or p_size = 14 or p_size = 27 or
+         p_size = 49 or p_size = 21 or p_size = 33 or
+         p_size = 35 or p_size = 28
+) q16_all
+group by 
+p_brand, 
+p_type, 
+p_size
+order by 
+supplier_cnt desc, 
+p_brand, 
+p_type, 
+p_size;
---
+++ queries/query.4.sql
@@ -0,0 +1,27 @@
+
+drop table if exists q4_order_priority_tmp;
+CREATE TABLE q4_order_priority_tmp (O_ORDERKEY INT);
+drop table if exists q4_order_priority;
+CREATE TABLE q4_order_priority (O_ORDERPRIORITY text, ORDER_COUNT INT);
+
+
+INSERT into q4_order_priority_tmp 
+select 
+  DISTINCT l_orderkey 
+from 
+  lineitem 
+where 
+  l_commitdate < l_receiptdate;
+  
+INSERT into q4_order_priority 
+select 
+	o_orderpriority, 
+	count(1) as order_count 
+from 
+  orders o join q4_order_priority_tmp t 
+  on 
+o.o_orderkey = t.o_orderkey 
+and o.o_orderdate >= '1996-05-01' 
+and o.o_orderdate < '1996-08-01' 
+group by o_orderpriority 
+order by o_orderpriority;
